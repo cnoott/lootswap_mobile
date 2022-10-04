07fa469d20bb20be4fc165243a3ff98236a9e723
Liam Amadio <liamamadio@gmail.com>
2022-10-04 08:40:47 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import NavBar from './shared/NavBar';
+import { NavigationContainer } from '@react-navigation/native';
+import { UserProvider } from './shared/UserContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <UserProvider>
+          <NavigationContainer>
+              <NavBar/>
+          </NavigationContainer>
+      </UserProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
