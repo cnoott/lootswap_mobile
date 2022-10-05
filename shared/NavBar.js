5b39c3a760fca78be8766dde5310c05e250f7fd5
Liam Amadio <liamamadio@gmail.com>
2022-10-05 17:17:06 -0500

    nav bar icons, signin and signup

--- shared/NavBar.js
+++ shared/NavBar.js
@@ -1,31 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Image } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Image, Text, View } from 'react-native';
 import HomeScreen from '../components/HomeScreen';
 import Profile from '../components/user/Profile';
 import AddLoot from '../components/AddLoot';
 import Orders from '../components/Orders';
 import Notifications from '../components/Notifications';
 import Logo from '../assets/logo.png';
+import Bell from '../assets/bell.png';
+import HomeIcon from '../assets/home.png';
+import AddLootIcon from '../assets/add-loot.png';
+import OrdersIcon from '../assets/orders.png';
+import OffersIcon from '../assets/offers.png';
+import ProfileIcon from '../assets/profile.png';
+
+
 import SplashScreen from '../shared/SplashScreen';
 import Signin from '../components/user/Signin';
+import Signup from '../components/user/Signup';
 import { isAuthenticated } from '../api/auth';
 
 import { useUserContext, useUserLoading } from './UserContext';
 
-import { HomeIcon } from 'react-native-heroicons/outline'
 
-const Tab = createBottomTabNavigator();
-
-const LogoImage = () => (
+const TopLogo = () => (
     <Image
         source={Logo}
         style={{ height: 35, width: 186 }}
     />
 );
 
-const Home = () => (
-    <HomeIcon/>
+const NotifButton = () => (
+    <Image
+        source={Bell}
+        style={{ height: 25, width: 25, right: 10}}
+    />
+);
+
+const TabIcon = ({source}) => (
+    <Image
+        source={source}
+        style={{ height: 23, width: 23}}
+    />
 );
 
 //TODO: 
@@ -35,72 +52,134 @@ const Home = () => (
 // - isSigned in state that shows different navigators
 // - splsh screen
 
-
 const NavBar = () => {
     const [loadingAuth, setLoadingAuth] = useState(true);
-    const [user, setUser] = useState(true);
 
     const userData = useUserContext();
     const userLoading = useUserLoading();
- 
-    
+
     if (userLoading) {
         return <SplashScreen/>
     }
 
+    const MainNavigator = () => {
+        const Tab = createBottomTabNavigator();
+        const BlankComponent = () => <View style={{ flex: 1, backgroundColor: 'white' }} />
+
+
+        return (
+            <Tab.Navigator
+                screenOptions={{ 
+                    headerTitle: () => <TopLogo/>,
+                    headerRight: () => <NotifButton/>
+                }}
+            >
+                { userData === null ? (
+                    <>
+                        <Tab.Screen 
+                            name='Home' 
+                            component={HomeScreen}
+                            options={{ tabBarIcon: () => <TabIcon source={HomeIcon} /> }}
+                        />
+
+                        <Tab.Screen 
+                            name='Add Loot' 
+                            component={BlankComponent}
+                            options={{ tabBarIcon: () => <TabIcon source={AddLootIcon} /> }}
+                            listeners={({ navigation }) => ({
+                                tabPress: e => {
+                                    e.preventDefault()
+                                    navigation.navigate('signin')
+                                },
+                            })}
+                        />
+                        <Tab.Screen 
+                            name='Orders' 
+                            component={BlankComponent}
+                            options={{ tabBarIcon: () => <TabIcon source={OrdersIcon} /> }}
+                            listeners={({ navigation }) => ({
+                                tabPress: e => {
+                                    e.preventDefault()
+                                    navigation.navigate('signin')
+                                },
+                            })}
+                        />
+                        <Tab.Screen 
+                            name='Offers' 
+                            component={BlankComponent}
+                            options={{ tabBarIcon: () => <TabIcon source={OffersIcon} /> }}
+                            listeners={({ navigation }) => ({
+                                tabPress: e => {
+                                    e.preventDefault()
+                                    navigation.navigate('signin')
+                                },
+                            })}
+                        />
+                        <Tab.Screen 
+                            name='Profile' 
+                            options={{ tabBarIcon: () => <TabIcon source={ProfileIcon} /> }}
+                            component={BlankComponent}
+                            listeners={({ navigation }) => ({
+                                tabPress: e => {
+                                    e.preventDefault()
+                                    navigation.navigate('signin')
+                                },
+                            })}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Tab.Screen 
+                            name='Home' 
+                            component={HomeScreen}
+                            options={{ tabBarIcon: () => <TabIcon source={HomeIcon} /> }}
+                        />
+
+                        <Tab.Screen 
+                            name='Add Loot' 
+                            component={AddLoot}
+                            options={{ tabBarIcon: () => <TabIcon source={AddLootIcon} /> }}
+                        />
+                        <Tab.Screen 
+                            name='Orders' 
+                            component={Orders}
+                            options={{ tabBarIcon: () => <TabIcon source={OrdersIcon} /> }}
+                        />
+                        <Tab.Screen 
+                            name='Offers' 
+                            component={Notifications}
+                            options={{ tabBarIcon: () => <TabIcon source={OffersIcon} /> }}
+                        />
+                        <Tab.Screen 
+                            name='Profile' 
+                            component={Profile}
+                            options={{ tabBarIcon: () => <TabIcon source={ProfileIcon} /> }}
+                        />
+                    </>
+                )}
+            </Tab.Navigator>
+        );
+    };
+    
+    const RootStack = createStackNavigator();
+
     return (
-        <Tab.Navigator
-            screenOptions={{ headerTitle: () => <LogoImage/>}}
+        <RootStack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
         >
-            { userData === null ? (
-                <>
-                <Tab.Screen 
-                    name='Home' 
-                    component={HomeScreen}
-                />
-                <Tab.Screen 
-                    name='Profile' 
-                    component={() => <Signin setUser={setUser}/>}
-                />
-                <Tab.Screen 
-                    name='Add Loot' 
-                    component={Signin}
-                />
-                <Tab.Screen 
-                    name='Orders' 
-                    component={Signin}
-                />
-                <Tab.Screen 
-                    name='Notifications' 
-                    component={Signin}
-                />
-                </>
-            ) : (
-                <>
-                <Tab.Screen 
-                    name='Home' 
-                    component={HomeScreen}
-                />
-                <Tab.Screen 
-                    name='Profile' 
-                    component={Profile}
-                />
-                <Tab.Screen 
-                    name='Add Loot' 
-                    component={AddLoot}
-                />
-                <Tab.Screen 
-                    name='Orders' 
-                    component={Orders}
-                />
-                <Tab.Screen 
-                    name='Notifications' 
-                    component={Notifications}
-                />
-                </>
-            )}
-        </Tab.Navigator>
+            <RootStack.Group>
+                <RootStack.Screen name='main' component={MainNavigator}/>
+            </RootStack.Group>
+
+            <RootStack.Group screenOptions={{ presentation: 'modal' }}>
+                <RootStack.Screen name='signin' component={Signin}/>
+                <RootStack.Screen name='signup' component={Signup}/>
+            </RootStack.Group>
+        </RootStack.Navigator>
     );
+
 };
 
 export default NavBar;
