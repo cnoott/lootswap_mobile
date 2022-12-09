ea0d6c44e18b544d26deb0f17f68d9fde3f8006e
Akhilesh Mourya <mouryaakhilesh62@gmail.com>
2022-12-09 11:34:18 +0530

    Created product card reusable component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,55 +5,27 @@
 import React, {FC} from 'react';
 import {InHomeHeader} from '../../components/commonComponents/headers/homeHeader';
 import CarouselComponent from '../../components/Carousel';
-import {
-  Container,
-  SubContainer,
-  FlatList,
-  ItemContainer,
-  Image,
-  FreeShipingContainer,
-  ShippingText,
-  CellBottomView,
-  BottomHeaderView,
-  HeaderTextMain,
-  EmptyRowView,
-  HeaderDes,
-} from './styles';
+import {Container, SubContainer, FlatList} from './styles';
 import algoliasearch from 'algoliasearch/lite';
 import {InstantSearch, useInfiniteHits} from 'react-instantsearch-hooks';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import LSSearch from '../../components/filterSearch';
 import {AlgoliaAppId, AlgoliaApiKey, ALGOLIA_INDEX_NAME} from '@env';
+import LSProductCard from '../../components/productCard';
+
 const searchClient = algoliasearch(AlgoliaAppId, AlgoliaApiKey);
 
 export const HomeScreen: FC<{}> = () => {
   const navigation: NavigationProp<any, any> = useNavigation(); // Accessing navigation object
-  const renderItem = ({item}) => {
-    return (
-      <ItemContainer
-        onPress={() => navigation.navigate('ProductDetailsScreen')}>
-        <Image source={{uri: item.primary_photo}} />
-        <CellBottomView>
-          <BottomHeaderView>
-            <EmptyRowView>
-              <HeaderTextMain>{item.brand}</HeaderTextMain>
-            </EmptyRowView>
-            <HeaderTextMain>${item.price}</HeaderTextMain>
-          </BottomHeaderView>
-          <HeaderDes>{item.name}</HeaderDes>
-          <EmptyRowView>
-            <HeaderTextMain>Size {item.size}</HeaderTextMain>
-          </EmptyRowView>
-        </CellBottomView>
 
-        {item.who_pays === 'seller-pays' && (
-          <FreeShipingContainer>
-            <ShippingText>Free Shipping</ShippingText>
-          </FreeShipingContainer>
-        )}
-      </ItemContainer>
-    );
+  const onProductPress = () => {
+    navigation.navigate('ProductDetailsScreen');
   };
+
+  const renderItem = ({item}) => {
+    return <LSProductCard item={item} onPress={onProductPress} />;
+  };
+
   const onRightIconPress = () => {
     navigation.navigate('HomeFiltersScreen');
   };
