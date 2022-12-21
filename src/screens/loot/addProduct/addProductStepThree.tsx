92ace45e86b0160f590530b06e655d5520f9b156
Akhilesh Mourya <mouryaakhilesh62@gmail.com>
2022-12-21 21:27:08 +0530

    Add Products title & step3

--- src/screens/loot/addProduct/addProductStepThree.tsx
+++ src/screens/loot/addProduct/addProductStepThree.tsx
@@ -3,10 +3,25 @@ LootSwap - ADD_PRODUCT STEP 1
 ***/
 
 import React, {FC} from 'react';
-import {Container} from './styles';
+import {Container, AddProductsList, ImageContainer} from './styles';
 
 export const AddProductStepThree: FC<{}> = () => {
-  return <Container />;
+  const renderProductImageContainer = () => {
+    return (
+      <ImageContainer>
+        {/* <Image source={{uri: 'https://picsum.photos/id/237/200/300'}} /> */}
+      </ImageContainer>
+    );
+  };
+  return (
+    <Container>
+      <AddProductsList
+        data={[...new Array(5).keys()]}
+        renderItem={renderProductImageContainer}
+        keyExtractor={item => item}
+      />
+    </Container>
+  );
 };
 
 export default AddProductStepThree;
