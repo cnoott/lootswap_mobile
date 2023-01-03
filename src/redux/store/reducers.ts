7fef2beb887f645488c2ef3d8ba9d8afca2c5bb7
Liam Amadio <liamamadio@gmail.com>
2023-01-03 15:27:49 -0600

    set up redux/saga for offers, now time for frontend integration

--- src/redux/store/reducers.ts
+++ src/redux/store/reducers.ts
@@ -6,6 +6,7 @@ import auth from '../modules/auth/reducer';
 import loading from '../modules/loading/reducer';
 import home from '../modules/home/reducer';
 import message from '../modules/message/reducer';
+import offers from '../modules/offers/reducer.ts';
 import {combineReducers} from 'redux';
 
 const appReducer = combineReducers({
@@ -13,6 +14,7 @@ const appReducer = combineReducers({
   loading,
   home,
   message,
+  offers,
 });
 
 const rootReducer = (state: any, action: any) => {
--- src/services/apiEndpoints.ts
+++ src/services/apiEndpoints.ts
@@ -117,6 +117,13 @@ export const createNewProductCall = (reqData: any) => {
   );
 };
 
+export const getTradesHistoryCall = (reqData: any) => {
+  return handleResponse(
+    api.get(`/trades/${reqData?.userId}`),
+    API_RESPONSE.CODE200,
+  );
+};
+
 const handleResponse = (call: any, code: any, detailErrorMsg?: any) => {
   return call
     .then((res: any) => {
