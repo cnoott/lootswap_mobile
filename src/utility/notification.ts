3d28652a27fc2ae6fc379d56e14930ea85b3a0bc
Liam Amadio <liamamadio@gmail.com>
2023-02-20 13:57:47 -0600

    trade-order notif handleNavigation

--- src/utility/notification.ts
+++ src/utility/notification.ts
@@ -1,4 +1,10 @@
-export const handleNavigation = (navigation: any, message: any) => {
+import {getOrder} from '../redux/modules';
+
+export const handleNavigation = (
+  navigation: any,
+  message: any,
+  dispatch: any,
+) => {
   switch (message.data.notifType) {
     case 'trade':
       navigation.reset({
@@ -23,5 +29,31 @@ export const handleNavigation = (navigation: any, message: any) => {
           messageId: message?.data?.objectId,
         },
       });
+      break;
+
+    case 'trade-order':
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Profile'}],
+      });
+      dispatch(
+        getOrder(
+          {orderId: message?.data?.objectId},
+          res => {
+            navigation.navigate('Profile', {
+              screen: 'TrackOrderScreen',
+              params: {
+                isTradeOrder: true,
+                item: res,
+              },
+            });
+          },
+          error => {
+            console.log(error);
+          },
+        ),
+      );
+    //TODO: rate notif type
+    //TODO: paypal-order notif type
   }
 };
