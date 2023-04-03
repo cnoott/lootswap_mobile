d25e66fde646dc514e8aacb0d1d265c6759a2b33
Liam Amadio <liamamadio@gmail.com>
2023-04-03 14:18:50 -0500

    changed referral screen text

--- src/screens/profile/referralScreen.tsx
+++ src/screens/profile/referralScreen.tsx
@@ -3,7 +3,7 @@
  ***/
 
 
-import React, {FC} from 'react';
+import React, {FC, useEffect, useCallback} from 'react';
 import {
   TopContainer,
   TopTextContainer,
@@ -24,8 +24,52 @@ import {SvgXml} from 'react-native-svg';
 import {InStackHeader} from '../../components/commonComponents/headers/stackHeader';
 import LSButton from '../../components/commonComponents/LSButton';
 import {Size, Type} from '../../enums';
+import {useSelector, useDispatch} from 'react-redux';
+import {AuthProps} from '../../redux/modules/auth/reducer';
+import branch from 'react-native-branch';
 
 export const ReferralScreen: FC<{}> = () => {
+  const auth: AuthProps = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  const {userData} = auth;
+
+  const generateReferralLink = useCallback(async () => {
+    if (!userData?.referralLink) {
+      let buo = await branch.createBranchUniversalObject(
+        `referral_${userData?._id}`,
+        {
+          title: 'lootswap referral link',
+          contentDescription: 'Share and get paid $5 for every new member you refer!',
+          contentMetadata: {
+            customMetadata: {
+              userId: userData?._id,
+            },
+          },
+        },
+      );
+
+      const linkProperties = {
+        feature: 'referral',
+        channel: 'in-app',
+        campaign: 'lootswap refer a friend',
+        stage: 'new user',
+        tags: ['referral'],
+      };
+
+      const controlParams = {
+        $desktop_url: 'https://download.lootswap.com',
+      };
+
+      const {url} = await buo.generateShortUrl(linkProperties, controlParams);
+      console.log('URL GENERATED: ', url);
+      //save url here
+    }
+  }, [userData?._id, userData?.referralLink]);
+
+  useEffect(() => {
+    generateReferralLink();
+  }, [generateReferralLink]);
+
   return (
     <Container>
       <TopContainer>
@@ -34,7 +78,7 @@ export const ReferralScreen: FC<{}> = () => {
         <TopTextContainer>
           <TopTextHeader>
             Get paid <GreenText>$5</GreenText> for every new member you refer
-            completing their first trade! 💸 🔀
+            after they complete their first trade! 💸 🔀
           </TopTextHeader>
           <TopTextSub>
             *compensation will be on your lootswap wallet.
