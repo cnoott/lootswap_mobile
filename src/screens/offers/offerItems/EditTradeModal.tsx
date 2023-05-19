06866c07e939c264df8926937f358daa2edef326
Liam Amadio <liamamadio@gmail.com>
2023-05-19 11:53:17 -0500

    chagned up the edit trade modal and removed the money offer and remove items modals

--- src/screens/offers/offerItems/EditTradeModal.tsx
+++ src/screens/offers/offerItems/EditTradeModal.tsx
@@ -16,9 +16,7 @@ import {
 interface EditTradeModalProp {
   isModalVisible: boolean;
   onCloseModal?: Function;
-  onAddItemPress?: Function;
-  onRemoveItemPress?: Function;
-  onChangeOfferPress?: Function;
+  onEditTradePress?: Function;
   offerItem: any;
   userData: any;
 }
@@ -27,9 +25,7 @@ export const EditTradeModal: FC<EditTradeModalProp> = props => {
   const {
     isModalVisible,
     onCloseModal = () => {},
-    onAddItemPress = () => {},
-    onRemoveItemPress = () => {},
-    onChangeOfferPress = () => {},
+    onEditTradePress = () => {},
     offerItem,
     userData,
   } = props;
@@ -40,36 +36,19 @@ export const EditTradeModal: FC<EditTradeModalProp> = props => {
       onBackdropPress={() => onCloseModal()}>
       <LSModal.BottomContainer>
         <TradeModalContainerView>
-          <ModalHeaderText>Edit Trade Options</ModalHeaderText>
+          <ModalHeaderText>Edit Trade</ModalHeaderText>
           <TopMargin />
           <>
             <LSButton
-              title={'Add Items'}
+              title={'Change Offer'}
               size={Size.Fit_To_Width}
               type={Type.Primary}
               radius={20}
               fitToWidth={'90%'}
-              onPress={() => onAddItemPress()}
-            />
-            <TopMargin margin={2} />
-            <LSButton
-              title={'Remove Items'}
-              size={Size.Fit_To_Width}
-              type={Type.Error}
-              radius={20}
-              fitToWidth={'90%'}
-              onPress={() => onRemoveItemPress()}
+              onPress={() => onEditTradePress()}
             />
             <TopMargin margin={2} />
           </>
-          <LSButton
-            title={'Add/Change Money Offer'}
-            size={Size.Fit_To_Width}
-            type={Type.Secondary}
-            radius={20}
-            fitToWidth={'90%'}
-            onPress={() => onChangeOfferPress()}
-          />
         </TradeModalContainerView>
         <LSModal.CloseButton onCloseButtonPress={() => onCloseModal()} />
       </LSModal.BottomContainer>
