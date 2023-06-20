ccb6aa7ac6ea5c17662d65d046deb8704328fb32
Liam Amadio <liamamadio@gmail.com>
2023-06-20 12:35:53 -0500

    eslint auto syntax fixes

--- src/components/loot/chooseAlbumDropDownStyles.ts
+++ src/components/loot/chooseAlbumDropDownStyles.ts
@@ -2,7 +2,6 @@ import {moderateScale, verticalScale, scale} from 'react-native-size-matters';
 import styled from 'styled-components/native';
 import {layout, space} from 'styled-system';
 
-
 export const DropdownStyle = {
   height: verticalScale(48),
   width: scale(100),
