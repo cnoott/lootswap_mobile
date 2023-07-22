13cc148029ad1a1e0f81e6eb665ec177b46cbda5
graslowsnail <graslowsnail@gmail.com>
2023-07-21 21:38:08 -0500

    updated the users notification settings screen to connect with the backend and update the users notification_settings obj using redux.

--- src/screens/profile/notificationSettings.tsx
+++ src/screens/profile/notificationSettings.tsx
@@ -2,11 +2,13 @@
 LootSwap - NOTIFICATION SETTINGS SCREEN
 ***/
 
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 import {Switch} from 'react-native';
+import {useDispatch,useSelector} from 'react-redux';
 import {useTheme} from 'styled-components';
 import {InStackHeader} from '../../components/commonComponents/headers/stackHeader';
-import {getNotificationSettingsList} from '../../utility/utility';
+import {updateUser} from '../../redux/modules';
+import {AuthProps} from '../../redux/modules/auth/reducer';
 import {
   Container,
   SubContainer,
@@ -14,23 +16,43 @@ import {
   ItemLabel,
 } from './notificationsSettingStyle';
 
+//TODO:
+// - DELETE the getNotificationSettingsList
+// - Make a local state for the current user settings
+//      > The initial state will be filled from
+// - Each switch will change the state
+// - Send the state in the updateUser function
+
 export const NotificationSettingScreen: FC<{}> = () => {
+  const auth: AuthProps = useSelector(state => state.auth);
+  const {userData} = auth || {};
+  const dispatch = useDispatch();
   const theme = useTheme();
-  const [settingsData, setSettingsData] = useState(
-    getNotificationSettingsList(),
-  );
-  const onToggleChange = id => {
-    const existingData = [...settingsData];
-    const newData = existingData.map(p =>
-      p.id === id ? {...p, status: !p.status} : p,
+
+  // destructure the notification_settings from userData
+  const {notification_settings} = userData || {};
+
+  const onToggleChange = (settingName,value) => {
+    const newNotificatonSettings = {
+      ...notification_settings,
+      [settingName]: value,
+    };
+    dispatch(
+      updateUser({
+        userId: userData?._id,
+        userData: {
+        ...userData,
+        notification_settings: newNotificatonSettings,
+        },
+      }),
     );
-    setSettingsData(newData);
   };
-  const renderItems = data => {
-    const isEnabled = data?.status;
+
+  const renderItems = (label, settingName) => {
+ const isEnabled = notification_settings?.[settingName];
     return (
-      <ItemContainer>
-        <ItemLabel>{data?.label}</ItemLabel>
+      <ItemContainer key={settingName}>
+        <ItemLabel>{label}</ItemLabel>
         <Switch
           trackColor={{
             false: theme.colors.grey,
@@ -38,7 +60,7 @@ export const NotificationSettingScreen: FC<{}> = () => {
           }}
           thumbColor={isEnabled ? theme.colors.white : theme.colors.white}
           ios_backgroundColor={theme.colors.grey}
-          onValueChange={() => onToggleChange(data?.id)}
+          onValueChange={value => onToggleChange(settingName,value)}
           value={isEnabled}
         />
       </ItemContainer>
@@ -48,9 +70,14 @@ export const NotificationSettingScreen: FC<{}> = () => {
     <Container>
       <InStackHeader title={'Notification Settings'} />
       <SubContainer>
-        {settingsData?.map(data => {
-          return renderItems(data);
-        })}
+        {notification_settings && (
+          <>
+            {renderItems('Trade Sales Updates', 'tradeSalesUpdates')}
+            {renderItems('Promos', 'promos')}
+            {renderItems('New Items', 'newItems')}
+            {renderItems('All Notifications', 'allNotifications')}
+          </>
+        )}
       </SubContainer>
     </Container>
   );
