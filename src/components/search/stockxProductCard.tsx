a0d063c57339e21e113b8b09954207500bb2b19d
Liam Amadio <liamamadio@gmail.com>
2023-09-18 15:11:53 -0500

    made the stockx product into its own reusable component

---
+++ src/components/search/stockxProductCard.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from 'react';
+import {
+  StockxContainer,
+  StockxImageContainer,
+  StockxImage,
+  StockxTextContainer,
+  StockxTitleText,
+  StockxNumListingsText,
+} from './stockxProductCardStyles';
+
+interface StockxProductCardProps {
+  stockxProduct: any;
+  foundProducts: Array<any>;
+  handleStockxNavigation?: Function;
+}
+
+export const StockxProductCard: FC<StockxProductCardProps> = (props) => {
+  const {
+    stockxProduct,
+    foundProducts,
+    handleStockxNavigation = () => {},
+  } = props;
+
+  return (
+    <StockxContainer onPress={() => handleStockxNavigation(stockxProduct, foundProducts)}>
+      <StockxImageContainer>
+        <StockxImage source={{uri: stockxProduct?.image}} />
+      </StockxImageContainer>
+      <StockxTextContainer>
+        <StockxTitleText>{stockxProduct?.name}</StockxTitleText>
+        <StockxNumListingsText>
+          {`${foundProducts?.length} listing${
+            foundProducts?.length !== 1 ? 's' : ''} avaliable`
+          }
+        </StockxNumListingsText>
+      </StockxTextContainer>
+    </StockxContainer>
+  );
+};
+
+export default StockxProductCard;
--- src/screens/search/styles.ts
+++ src/screens/search/styles.ts
@@ -107,71 +107,7 @@ export const ClearRecentSearchesText = styled.Text.attrs(props => ({
   ${space}
 `;
 
-export const StockxContainer = styled.TouchableOpacity.attrs(props => ({
-  width: scale(225),
-  alignSelf: 'stretch',
-  borderRadius: 9,
-  bg: props.theme.colors.white,
-  mr: scale(5),
-  my: scale(30),
-}))`
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  ${layout} ${color} ${space} ${border};
-`;
 
-export const StockxImageContainer = styled.View.attrs(props => ({
-  height: scale(82),
-  width: scale(82),
-  bg: props.theme.colors.white,
-  borderRadius: scale(14),
-}))`
-  align-items: center;
-  justify-content: center;
-  ${layout} ${color} ${space} ${border};
-`;
-
-export const StockxTextContainer = styled.View.attrs(() => ({
-  flex: 1,
-  flexDirection: 'column',
-  mt: verticalScale(2),
-}))`
-  justify-content: center;
-  ${color}
-  ${space}
-  ${layout}
-`;
-
-export const StockxTitleText = styled.Text.attrs(props => ({
-  color: props.theme.colors.black,
-  numberOfLines: 2,
-  mt: verticalScale(5),
-  ml: 2,
-}))`
-  font-size: ${moderateScale(14)}px;
-  font-family: Urbanist;
-  font-weight: 600;
-  text-align: left;
-  ${color}
-  ${space}
-  ${layout}
-`;
-
-export const StockxNumListingsText = styled.Text.attrs(props => ({
-  color: props.theme.colors.textGrey,
-  numberOfLines: 2,
-  mt: verticalScale(5),
-  ml: 2,
-}))`
-  font-size: ${moderateScale(12)}px;
-  font-family: Urbanist;
-  font-weight: 600;
-  text-align: left;
-  ${color}
-  ${space}
-  ${layout}
-`;
 
 export const StockxBrandContainer = styled.View.attrs(() => ({
   flex: 1,
@@ -182,18 +118,6 @@ export const StockxBrandContainer = styled.View.attrs(() => ({
   flex-direction: row ${space};
 `;
 
-export const StockxImage = styled(FastImage).attrs(() => ({
-  resizeMode: 'contain',
-  width: moderateScale(85),
-  height: moderateScale(85),
-  borderRadius: scale(13),
-}))`
-  position: absolute;
-  align-self: center;
-  ${color}
-  ${space}
-  ${layout}
-`;
 
 export const StockxFlatList = styled.FlatList.attrs(() => ({
   horizontal: true,
