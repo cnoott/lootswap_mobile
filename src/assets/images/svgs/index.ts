547aa5438203a2952c886f0009cc35382ac0b5fb
Liam Amadio <liamamadio@gmail.com>
2023-09-20 16:48:04 -0500

    changed filter button color

--- src/assets/images/svgs/index.ts
+++ src/assets/images/svgs/index.ts
@@ -214,10 +214,10 @@ export const HOME_SEARCH_INPUT_ICON_SELECTED = `<svg width="20" height="20" view
 </svg>`;
 
 export const HOME_FILTER_ICON = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.60802 13.8288H3.35742" stroke="#101010" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M10.9502 5.75072H16.2008" stroke="#101010" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M7.27158 5.70521C7.27158 4.6255 6.38978 3.75 5.30229 3.75C4.21481 3.75 3.33301 4.6255 3.33301 5.70521C3.33301 6.78492 4.21481 7.66042 5.30229 7.66042C6.38978 7.66042 7.27158 6.78492 7.27158 5.70521Z" stroke="#101010" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M16.6661 13.7911C16.6661 12.7114 15.785 11.8359 14.6975 11.8359C13.6093 11.8359 12.7275 12.7114 12.7275 13.7911C12.7275 14.8709 13.6093 15.7464 14.6975 15.7464C15.785 15.7464 16.6661 14.8709 16.6661 13.7911Z" stroke="#101010" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.60802 13.8288H3.35742" stroke="#6267FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.9502 5.75072H16.2008" stroke="#6267FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M7.27158 5.70521C7.27158 4.6255 6.38978 3.75 5.30229 3.75C4.21481 3.75 3.33301 4.6255 3.33301 5.70521C3.33301 6.78492 4.21481 7.66042 5.30229 7.66042C6.38978 7.66042 7.27158 6.78492 7.27158 5.70521Z" stroke="#6267FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16.6661 13.7911C16.6661 12.7114 15.785 11.8359 14.6975 11.8359C13.6093 11.8359 12.7275 12.7114 12.7275 13.7911C12.7275 14.8709 13.6093 15.7464 14.6975 15.7464C15.785 15.7464 16.6661 14.8709 16.6661 13.7911Z" stroke="#6267FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 `;
 
