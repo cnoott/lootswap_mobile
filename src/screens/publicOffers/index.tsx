654091de94deb4bf454a04ef10a1e0a720774678
Liam Amadio <liamamadio@gmail.com>
2023-09-25 13:57:16 -0500

    inital public offers screen + navigation

---
+++ src/screens/publicOffers/index.tsx
@@ -0,0 +1,59 @@
+/***
+LootSwap - CREATE PUBLIC OFFER
+***/
+
+import React, {FC, useState, useRef} from 'react';
+import {ProgressBar, SwiperComponent} from '../loot/styles';
+import {LSStartTradeHeader} from '../../components/commonComponents/headers/startTradeHeader';
+import LSButton from '../../components/commonComponents/LSButton';
+import {Size, Type} from '../../enums';
+import {
+  Container,
+  ButtonContainer,
+} from './styles';
+
+const NUMBER_OF_STEPS = 4;
+
+export const CreatePublicOfferScreen: FC<any> = () => {
+  const swiperRef = useRef<any>(null);
+  const [currPage, setCurrPage] = useState(0);
+  const handleBack = () => {};
+  const handleNext = () => {};
+
+  const renderTopView = () => (
+    <>
+      <LSStartTradeHeader
+        title={'Public Offers'}
+        profilePicture={''}
+        showPfp={false}
+        onBackPress={handleBack}
+      />
+      <ProgressBar progress={(currPage + 1) / NUMBER_OF_STEPS} />
+    </>
+  );
+
+  const renderBottomButtonView = () =>
+    currPage !== 3 && (
+      <ButtonContainer>
+        <LSButton
+          title={'Next'}
+          size={Size.Large}
+          type={Type.Primary}
+          radius={20}
+          onPress={handleNext}
+        />
+      </ButtonContainer>
+    );
+
+
+  return (
+    <Container>
+      {renderTopView()}
+      <SwiperComponent ref={swiperRef} onIndexChanged={setCurrPage}>
+
+      </SwiperComponent>
+    </Container>
+  );
+};
+
+export default CreatePublicOfferScreen;
