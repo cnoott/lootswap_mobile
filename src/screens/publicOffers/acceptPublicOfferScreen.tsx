d14077a293c2bc0826f581428d4917118a2c4572
Liam Amadio <liamamadio@gmail.com>
2023-10-10 17:29:19 -0500

    initial accept public offer checkout

---
+++ src/screens/publicOffers/acceptPublicOfferScreen.tsx
@@ -0,0 +1,20 @@
+/***
+LootSwap - ACCEPT PUBLIC OFFER SCREEN
+***/
+
+import React, {FC, useState, useEffect, useCallback} from 'react';
+
+type PaymentDetails = {
+  platformFee: number;
+  toUserRate: number;
+  toWarehouseRate: number;
+  total: number;
+  userPayout: number;
+};
+
+export const AcceptPublicOfferScreen: FC<any> = ({route}) => {
+
+
+};
+
+export default AcceptPublicOfferScreen;
