28f478dade541083bbcb25021d47445f96f7944f
Liam Amadio <liamamadio@gmail.com>
2023-11-03 16:42:26 -0500

    show notif badge on orders

--- src/components/orders/orderTradeOrdersCell.tsx
+++ src/components/orders/orderTradeOrdersCell.tsx
@@ -3,6 +3,7 @@ import TradeOfferCell from '../../screens/offers/offerItems/TradeOfferCell';
 import PublicOfferCell from '../../components/publicOffer/PublicOfferCell'; 
 import {PurchaseCellContainer} from './styles';
 import TradeOrderUserDetailView from './tradeOrderUserDetailView';
+import CellBadge from '../../components/offers/cellBadge';
 
 interface OrderPurchaseProps {
   item?: any;
@@ -12,9 +13,14 @@ interface OrderPurchaseProps {
 
 function OrderTradeOrdersCell(props: OrderPurchaseProps) {
   const {item, onCellPress = () => {}, userData} = props;
+  const isReciever = item.reciever._id === userData?._id;
+  const showNotifBadge =
+    (isReciever && item.recieverNewNotif) ||
+    (!isReciever && item.senderNewNotif)
   return (
     <PurchaseCellContainer onPress={() => onCellPress(true)}>
       <TradeOrderUserDetailView item={item} userData={userData} />
+      {showNotifBadge && <CellBadge top={15} left={8}/>}
       {item.tradeId ? (
         <TradeOfferCell offerItem={item?.tradeId} />
       ) : (
