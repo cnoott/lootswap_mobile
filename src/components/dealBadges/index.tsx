c844a63e151985916dab07ef46d0f9b3d8b7a4c1
Liam Amadio <liamamadio@gmail.com>
2023-12-15 15:30:29 -0600

    deal badge disabled

--- src/components/dealBadges/index.tsx
+++ src/components/dealBadges/index.tsx
@@ -7,84 +7,54 @@ import {SvgXml} from 'react-native-svg';
 import {findMarketDataFromSize} from '../../utility/utility';
 
 interface DealBadgeProps {
-  dealType: typeof Deal_Type;
   fromProductPage: Boolean;
   item: any;
 }
 export const DealBadge: FC<DealBadgeProps> = props => {
-  const {dealType, fromProductPage, item} = props;
+  const {fromProductPage, item} = props;
 
-  // $501 - $800
-  const upperRangeDealType = () => {
-    const {lastSale} = findMarketDataFromSize(item, item.size);
-    const itemPrice = parseFloat(item.price);
-    const marketPrice = parseFloat(lastSale);
-
-    if (itemPrice >= marketPrice + marketPrice + 0.1) {
+  const getDealType = (
+    itemPrice: number,
+    marketPrice: number,
+    upperThreshold: number,
+    lowerThreshold: number,
+  ) => {
+    if (itemPrice > marketPrice + marketPrice * upperThreshold) {
       return Deal_Type.Bad;
     }
 
     if (
-      itemPrice > marketPrice + marketPrice * 0.04 &&
-      itemPrice <= marketPrice + marketPrice * 0.1
+      itemPrice > marketPrice + marketPrice * lowerThreshold &&
+      itemPrice <= marketPrice + marketPrice * upperThreshold
     ) {
       return Deal_Type.Fair;
     }
 
-    if (itemPrice <= marketPrice + marketPrice * 0.4) {
+    if (itemPrice <= marketPrice + marketPrice * lowerThreshold) {
       return Deal_Type.Great;
     }
 
-    return Deal_Type.Fair;
+    return undefined;
   };
 
-  // $200 - $500
-  const getMidRangeDealType = () => {
-    const {lastSale} = findMarketDataFromSize(item, item.size);
+  const getDealRange = () => {
+    const {lastSale} = findMarketDataFromSize(item.stockxId, item.size);
+    console.log('LAST SALE', lastSale);
+    //const lastSale = 200;
     const itemPrice = parseFloat(item.price);
     const marketPrice = parseFloat(lastSale);
-
-    if (itemPrice > marketPrice + marketPrice + 0.15) {
-      return Deal_Type.Bad;
-    }
-
-    if (
-      itemPrice > marketPrice + marketPrice * 0.05 &&
-      itemPrice <= marketPrice + marketPrice * 0.15
-    ) {
-      return Deal_Type.Fair;
-    }
-
-    if (itemPrice <= marketPrice + marketPrice * 0.05) {
-      return Deal_Type.Great;
-    }
-
-    return Deal_Type.Fair;
-  };
-
-  // $1 - 199
-  const lowerRangeDealType = () => {
-    const {lastSale} = findMarketDataFromSize(item, item.size);
-    const itemPrice = parseFloat(item.price);
-    const marketPrice = parseFloat(lastSale);
-
-    if (itemPrice > marketPrice + marketPrice + 0.2) {
-      return Deal_Type.Bad;
-    }
-
-    if (
-      itemPrice > marketPrice + marketPrice * 0.1 &&
-      itemPrice <= marketPrice + marketPrice * 0.2
-    ) {
-      return Deal_Type.Fair;
-    }
-
-    if (itemPrice <= marketPrice + marketPrice * 0.1) {
-      return Deal_Type.Great;
+    switch (true) {
+      case marketPrice < 199:
+        return getDealType(itemPrice, marketPrice, 0.2, 0.01);
+      case marketPrice > 199 && marketPrice < 500:
+        return getDealType(itemPrice, marketPrice, 0.15, 0.05);
+      case marketPrice > 500:
+        return getDealType(itemPrice, marketPrice, 0.1, 0.04);
     }
   };
 
   const colorAndText = () => {
+    const dealType = getDealRange();
     switch (dealType) {
       case Deal_Type.Great:
         return {
