9bb08f0bb639c1e17a907882e112709a7d015491
Liam Amadio <liamamadio@gmail.com>
2023-12-15 18:01:44 -0600

    refactored edit address

---
+++ src/components/profile/editAddressComponent.tsx
@@ -0,0 +1,158 @@
+import React, {FC, useEffect, useState, useRef} from 'react';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_KEY} from '@env';
+import {getMyDetailsRequest, editShippingAddr} from '../../redux/modules';
+import {
+  EmptyTopView,
+  EmptyBottomView,
+  AutocompleteStyles,
+  TopSpaceView,
+  DisclaimerView,
+  DisclaimerDesView,
+  DisclaimerTopLabel,
+} from '../../screens/profile/editProfileStyles';
+import {Size, Type} from '../../enums';
+import {SvgXml} from 'react-native-svg';
+import LSInput from '../../components/commonComponents/LSInput';
+import LSButton from '../../components/commonComponents/LSButton';
+import {useSelector, useDispatch} from 'react-redux';
+import {AuthProps} from '../../redux/modules/auth/reducer';
+import {Alert} from 'custom_top_alert';
+import {useNavigation, NavigationProp} from '@react-navigation/native';
+import {BOTTOM_TAB_HOME} from 'localsvgimages';
+
+interface EditAddressComponentProps {
+  isFromAddLoot?: Boolean;
+}
+
+export const EditAddressComponent: FC<EditAddressComponentProps> = props => {
+  const {isFromAddLoot = false} = props;
+  const dispatch = useDispatch();
+  const auth: AuthProps = useSelector(state => state.auth);
+  const {userData} = auth;
+  const [shippingInfo, setShippingInfo] = useState(userData?.shipping_address);
+  const {name, street1, street2, city, state, zip} = shippingInfo;
+  const ref = useRef();
+  const navigation: NavigationProp<any, any> = useNavigation();
+
+  useEffect(() => {
+    const addressText = [street1, city, state, zip]
+      .filter(Boolean)
+      .join(', ')
+      .trim();
+
+    ref.current?.setAddressText(addressText);
+  }, [userData?.shipping_address]);
+
+  const validation = () => {
+    if (!name || !street1 || !city || !state || !zip) {
+      Alert.showError('Please fill all information');
+      return false;
+    }
+    return true;
+  };
+
+  const onSubmit = () => {
+    const reqData = {
+      userId: userData?._id,
+      address: shippingInfo,
+    };
+    if (!validation()) {
+      return;
+    }
+    dispatch(
+      editShippingAddr(
+        reqData,
+        () => {
+          Alert.showSuccess('Saved!');
+          dispatch(getMyDetailsRequest(userData?._id));
+          if (isFromAddLoot) {
+            navigation.reset({
+              index: 0,
+              routes: [{name: 'Home'}],
+            });
+            navigation.navigate('Add loot', {
+              screen: 'LootScreen',
+            });
+          }
+        },
+        error => {
+          Alert.showError(error);
+        },
+      ),
+    );
+  };
+
+  const handleAutofillChange = (data: any, details: any) => {
+    const address = {
+      street1: data.terms[0].value + ' ' + data.terms[1].value,
+      city: data.terms[2].value,
+      state: data.terms[3].value,
+      zip: details.address_components.find(component =>
+        component.types.includes('postal_code'),
+      ).short_name,
+    };
+    console.log(address);
+    setShippingInfo({...shippingInfo, ...address});
+  };
+
+  const handleChange = (attr: string) => (value: any) => {
+    setShippingInfo({...shippingInfo, [attr]: value});
+  };
+
+  return (
+    <>
+      <EmptyTopView>
+        {isFromAddLoot && (
+          <DisclaimerView>
+            <SvgXml xml={BOTTOM_TAB_HOME} />
+            <DisclaimerTopLabel>
+              In order to continue listing an item, you must enter your shipping info
+            </DisclaimerTopLabel>
+          </DisclaimerView>
+        )}
+        <LSInput
+          onChangeText={handleChange('name')}
+          value={name}
+          placeholder={'Full Name'}
+        />
+        <GooglePlacesAutocomplete
+          ref={ref}
+          fetchDetails={true}
+          placeholder="Type your address"
+          onPress={(data, details) => {
+            console.log(JSON.stringify(data));
+            console.log('DETAILs', JSON.stringify(details));
+            handleAutofillChange(data, details);
+          }}
+          query={{
+            key: GOOGLE_MAPS_KEY,
+            language: 'en',
+            components: 'country:us',
+
+          }}
+          styles={AutocompleteStyles}
+          enablePoweredByContainer={false}
+        />
+        <TopSpaceView>
+          <LSInput
+            onChangeText={handleChange('street2')}
+            value={street2}
+            placeholder={'Street 2'}
+          />
+        </TopSpaceView>
+      </EmptyTopView>
+      <EmptyBottomView>
+        <LSButton
+          title={'Save'}
+          size={Size.Full}
+          type={Type.Primary}
+          radius={20}
+          onPress={onSubmit}
+        />
+      </EmptyBottomView>
+    </>
+  );
+};
+
+export default EditAddressComponent;
--- src/screens/loot/lootEditAddressScreen.tsx
+++ src/screens/loot/lootEditAddressScreen.tsx
@@ -2,146 +2,19 @@
 LootSwap - ADD LOOT EDIT ADDRESS SCREEN
 ***/
 
-import React, {FC, useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import React, {FC} from 'react';
 import {InStackHeader} from '../../components/commonComponents/headers/stackHeader';
-import LSInput from '../../components/commonComponents/LSInput';
-import LSButton from '../../components/commonComponents/LSButton';
-import {AuthProps} from '../../redux/modules/auth/reducer';
-import {getMyDetailsRequest, editShippingAddr} from '../../redux/modules';
-import {SvgXml} from 'react-native-svg';
-import {BOTTOM_TAB_HOME} from 'localsvgimages';
-
-import {Size, Type} from '../../enums';
-import {Formik} from 'formik';
-import {Alert} from 'custom_top_alert';
-import {
-  Container,
-  EmptyTopView,
-  EmptyBottomView,
-  Innercontainer,
-} from '../profile/editProfileStyles';
-import {
-  GuarenteedView,
-  GuarenteedDesView,
-  ProtectionTopLabel,
-  ProtectionBottomLabel,
-  ProtectionIconStyle,
-} from '../productDetails/styles';
-import {useNavigation, NavigationProp} from '@react-navigation/native';
-
-//TODO: "Buyer prodteciton badge" for expalning that addr is req
+import {Container, MainContent} from '../profile/editProfileStyles';
+import {EditAddressComponent} from '../../components/profile/editAddressComponent';
 
 export const LootEditAddressScreen: FC<{}> = () => {
-  const dispatch = useDispatch();
-  const auth: AuthProps = useSelector(state => state.auth);
-  const {userData} = auth;
-  const navigation: NavigationProp<any, any> = useNavigation(); // Accessing navigation object
-
-  const onSubmit = (values: FormProps) => {
-    const reqData = {
-      userId: userData?._id,
-      address: values,
-    };
-    dispatch(
-      editShippingAddr(
-        reqData,
-        () => {
-          Alert.showSuccess('Saved!');
-          dispatch(getMyDetailsRequest(userData?._id));
-          navigation.reset({
-            index: 0,
-            routes: [{name: 'Home'}],
-          });
-          navigation.navigate('List loot', {
-            screen: 'LootScreen',
-          });
-        },
-        error => {
-          Alert.showError(error);
-        },
-      ),
-    );
-  };
-
   return (
-    <KeyboardAwareScrollView
-      scrollEnabled={false}
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={Innercontainer}
-      keyboardShouldPersistTaps={'handled'}>
-      <Formik
-        initialValues={{
-          street1: userData?.shipping_address?.street1,
-          street2: userData?.shipping_address?.street2,
-          name: userData?.shipping_address?.name,
-          city: userData?.shipping_address?.city,
-          state: userData?.shipping_address?.state,
-          zip: userData?.shipping_address?.zip,
-        }}
-        validateOnChange={false}
-        validateOnBlur={true}
-        onSubmit={values => onSubmit(values)}>
-        {({handleChange, handleSubmit, errors, values}) => {
-          return (
-            <Container>
-              <EmptyTopView>
-                <InStackHeader title="Shipping Address" />
-                <GuarenteedView style={{marginHorizontal: 20}}>
-                  <SvgXml xml={BOTTOM_TAB_HOME} style={ProtectionIconStyle} />
-                  <GuarenteedDesView>
-                    <ProtectionBottomLabel>
-                      In order to continue listing an item, you must enter your
-                      shipping info
-                    </ProtectionBottomLabel>
-                  </GuarenteedDesView>
-                </GuarenteedView>
-                <LSInput
-                  onChangeText={handleChange('name')}
-                  value={values.name}
-                  placeholder={'Full Name'}
-                />
-                <LSInput
-                  onChangeText={handleChange('street1')}
-                  value={values.street1}
-                  placeholder={'Street 1'}
-                />
-                <LSInput
-                  onChangeText={handleChange('street2')}
-                  value={values.street2}
-                  placeholder={'Apartment/Unit (optional)'}
-                />
-                <LSInput
-                  onChangeText={handleChange('city')}
-                  value={values.city}
-                  placeholder={'City'}
-                />
-                <LSInput
-                  onChangeText={handleChange('state')}
-                  value={values.state}
-                  placeholder={'State (AL)'}
-                />
-                <LSInput
-                  onChangeText={handleChange('zip')}
-                  value={values.zip}
-                  placeholder={'Zip'}
-                />
-              </EmptyTopView>
-              <EmptyBottomView>
-                <LSButton
-                  title={'Continue'}
-                  size={Size.Full}
-                  type={Type.Primary}
-                  radius={20}
-                  onPress={handleSubmit}
-                />
-              </EmptyBottomView>
-            </Container>
-          );
-        }}
-      </Formik>
-    </KeyboardAwareScrollView>
+    <MainContent>
+      <InStackHeader title="Shipping Address"/>
+      <Container>
+        <EditAddressComponent isFromAddLoot={true}/>
+      </Container>
+    </MainContent>
   );
 };
 
