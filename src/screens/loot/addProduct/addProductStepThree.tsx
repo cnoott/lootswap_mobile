08db4274ff6d1ff217c4ee72bcf18f56d914070a
Liam Amadio <liamamadio@gmail.com>
2024-01-04 11:35:55 -0600

    add loot category undefined at the start

--- src/screens/loot/addProduct/addProductStepThree.tsx
+++ src/screens/loot/addProduct/addProductStepThree.tsx
@@ -331,7 +331,7 @@ export const AddProductStepThree: FC<ProductStep> = props => {
       <ImageGuideComponent
         isVisible={isImageGuideVisible}
         onClose={closeImageGuide}
-        category={addProductData?.stepOne?.category.value}
+        category={addProductData?.stepOne?.category?.value}
       />
       <EditPhotoModal
         isVisible={editModalVisible}
