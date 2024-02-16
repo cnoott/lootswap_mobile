5d1270b6c076fdfa69f1fbd9b5bfbd88c1b231ee
Liam Amadio <liamamadio@gmail.com>
2024-02-16 16:35:42 -0600

    refactored google signin button

---
+++ src/components/signInButtons/GoogleButton.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Size, Type} from '../../enums';
+import LSButton from '../commonComponents/LSButton';
+import {GOOGLE_ICON} from 'localsvgimages';
+import {useDispatch} from 'react-redux';
+import {signInWithGoogleRequest} from '../../redux/modules';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+interface GoogleButtonProps {
+  fcmToken?: any;
+  referringUserId?: string;
+}
+
+function GoogleButton(props: GoogleButtonProps) {
+  const {fcmToken = '', referringUserId = ''} = props;
+  const dispatch = useDispatch();
+
+  const googleSignUp = async () => {
+    const userInfo = await GoogleSignin.signIn();
+    console.log('token', fcmToken.token);
+
+    dispatch(
+      signInWithGoogleRequest({
+        ...userInfo,
+        userData: userInfo.user,
+        fcmToken: fcmToken.token,
+        referringUserId: referringUserId,
+      }),
+    );
+  };
+
+  return (
+    <LSButton
+      title={'Continue with Google'}
+      size={Size.Full}
+      type={Type.Grey}
+      radius={30}
+      icon={GOOGLE_ICON}
+      onPress={googleSignUp}
+    />
+  );
+}
+
+export default GoogleButton;
