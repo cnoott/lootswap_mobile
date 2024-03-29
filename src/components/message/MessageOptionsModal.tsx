f2ee4141d943d411257f4564366ff50092e3046c
Liam Amadio <liamamadio@gmail.com>
2024-03-29 13:50:54 -0500

    message button options

--- src/components/message/MessageOptionsModal.tsx
+++ src/components/message/MessageOptionsModal.tsx
@@ -17,11 +17,56 @@ interface MessageOptionsModalProps {
   isModalVisible: boolean;
   onCloseModal: Function;
   onSendOfferPress: Function;
+  onBuyNowPress: Function;
+  productDetails: any;
 }
 
 export const MessageOptionsModal: FC<MessageOptionsModalProps> = props => {
-  const {isModalVisible, onCloseModal = () => {}, onSendOfferPress} = props;
+  const {
+    isModalVisible,
+    onCloseModal = () => {},
+    onSendOfferPress = () => {},
+    onBuyNowPress = () => {},
+    productDetails = {},
+  } = props;
 
+  // XXX This code (sort of) repeats itself in productDetails
+  const renderButtons = () => {
+    if (!productDetails?.isVisible) {
+      return (
+        <LSButton
+          title={'Item No Longer Available'}
+          size={Size.Full}
+          type={Type.View}
+          radius={20}
+          onPress={() => {}}
+        />
+      );
+    }
+
+    return (
+      <>
+        <LSButton
+          title={'Send Offer'}
+          size={Size.Full}
+          type={Type.Primary}
+          radius={20}
+          onPress={onSendOfferPress}
+        />
+        <TopMargin />
+        {productDetails?.type !== 'trade-only' && (
+          <LSButton
+            title={'Buy Now'}
+            size={Size.Fit_To_Width}
+            type={Type.Secondary}
+            radius={20}
+            fitToWidth={'90%'}
+            onPress={onBuyNowPress}
+          />
+        )}
+      </>
+    );
+  };
   return (
     <LSModal
       isVisible={isModalVisible}
@@ -31,16 +76,7 @@ export const MessageOptionsModal: FC<MessageOptionsModalProps> = props => {
         <ModalContainerView>
           <ModalHeaderText>Message Options</ModalHeaderText>
           <TopMargin />
-          <>
-            <LSButton
-              title={'Send Offer'}
-              size={Size.Fit_To_Width}
-              type={Type.Primary}
-              radius={20}
-              fitToWidth={'90%'}
-              onPress={onSendOfferPress}
-            />
-          </>
+          {renderButtons()}
         </ModalContainerView>
         <LSModal.CloseButton onCloseButtonPress={() => onCloseModal()} />
       </LSModal.BottomContainer>
