2b015e21ea9e7c7c99d87d3ed71f6de5db8ded4c
Liam Amadio <liamamadio@gmail.com>
2024-04-08 20:15:26 -0500

    share icons horiztonal scroll view

--- src/components/product/ProductShareModal.tsx
+++ src/components/product/ProductShareModal.tsx
@@ -3,8 +3,22 @@ import {LSModal} from '../commonComponents/LSModal';
 import {
   ModalStyles,
   ModalHeaderText,
+  ModalSubText,
+  ScrollView,
+  Image,
+  IconTouchable,
+  IconText,
 } from './ProductShareModalStyles';
-import {ScrollView} from 'react-native';
+import {
+  IG_SHARE,
+  IG_STORIES_SHARE,
+  IMESSAGE,
+  COPY_LINK_ICON,
+} from '../../constants/imageConstants';
+import {SNAPCHAT_ICON, IMESSAGE_ICON} from 'localsvgimages';
+import {SvgXml} from 'react-native-svg';
+import {scale} from 'react-native-size-matters';
+
 
 interface ProductShareModalProps {
   isVisible: Boolean;
@@ -26,8 +40,31 @@ export const ProductShareModal: FC<ProductShareModalProps> =
       onBackdropPress={onCloseModal}>
       <LSModal.BottomContainer>
         <ModalHeaderText>Share Listing</ModalHeaderText>
+        <ModalSubText>Share To</ModalSubText>
+        <ScrollView horizontal>
+          <IconTouchable>
+            <SvgXml xml={SNAPCHAT_ICON} width={scale(60)} height={scale(60)} />
+            <IconText>Snapchat</IconText>
+          </IconTouchable>
+          <IconTouchable>
+            <Image source={IG_STORIES_SHARE}/>
+            <IconText>Stories</IconText>
+          </IconTouchable>
+          <IconTouchable>
+            <Image source={IG_SHARE}/>
+            <IconText>Instagram</IconText>
+          </IconTouchable>
+
+          <IconTouchable>
+            <Image source={IMESSAGE}/>
+            <IconText>iMessage</IconText>
+          </IconTouchable>
+          <IconTouchable>
+            <Image source={COPY_LINK_ICON}/>
+            <IconText>Copy Link</IconText>
+          </IconTouchable>
+        </ScrollView>
         <LSModal.CloseButton onCloseButtonPress={onCloseModal} />
-        {/* HOriztonal scroll */}
       </LSModal.BottomContainer>
     </LSModal>
   );
