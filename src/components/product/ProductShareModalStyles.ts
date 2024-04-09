703bdb1f7d4365b22ad9e6171ea35ca252d48ecd
Liam Amadio <liamamadio@gmail.com>
2024-04-08 20:15:26 -0500

    share icons horiztonal scroll view

--- src/components/product/ProductShareModalStyles.ts
+++ src/components/product/ProductShareModalStyles.ts
@@ -9,10 +9,57 @@ export const ModalStyles = {
 };
 export const ModalHeaderText: any = styled.Text.attrs(props => ({
   color: props.theme.colors.text,
+  mb: scale(15),
 }))`
   font-size: ${moderateScale(20)}px;
   font-family: Urbanist-Bold;
   text-align: center;
+  ${color}
+  ${space}
+  ${layout}
+`;
+
+export const ModalSubText: any = styled.Text.attrs(props => ({
+  color: props.theme.colors.text,
+}))`
+  font-size: ${moderateScale(18)}px;
+  font-family: Urbanist-Bold;
+  ${color}
+  ${space}
+  ${layout}
+`;
+
+export const ScrollView: any = styled.ScrollView.attrs(() => ({
+}))`
+  padding-vertical: ${scale(8)}px;
+  padding-bottom: ${scale(20)}px;
+  marginBottom: ${scale(35)}px;
+`;
+
+export const Image = styled.Image.attrs((props: any) => ({
+  width: scale(56),
+  height: scale(56),
+  borderRadius: scale(10)
+}))`
+  ${color}
+  ${space}
+  ${layout}
+`;
+
+export const IconTouchable = styled.TouchableOpacity.attrs(() => ({
+}))`
+
+  padding-left: ${scale(15)}px;
+`;
+
+export const IconText: any = styled.Text.attrs(props => ({
+  color: props.theme.colors.text,
+  mt: scale(5),
+}))`
+  font-size: ${moderateScale(10)}px;
+  font-family: Urbanist;
+  text-align: center;
+  align-items: center;
   ${color}
   ${space}
   ${layout}
