2c45fe85e089f5f023df44f45785df6a18e93af1
Liam Amadio <liamamadio@gmail.com>
2024-04-11 16:16:11 -0400

    view shot integration with snapchat

--- src/components/product/ProductShareModal.tsx
+++ src/components/product/ProductShareModal.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useRef} from 'react';
 import {LSModal} from '../commonComponents/LSModal';
 import {
   ModalStyles,
@@ -18,7 +18,11 @@ import {
 import {SNAPCHAT_ICON, IMESSAGE_ICON} from 'localsvgimages';
 import {SvgXml} from 'react-native-svg';
 import {scale} from 'react-native-size-matters';
+import {CreativeKit} from '@snapchat/snap-kit-react-native';
+import ViewShot from 'react-native-view-shot';
+import LSProductCard from '../productCard';
 
+import {Share, View} from 'react-native';
 
 interface ProductShareModalProps {
   isVisible: Boolean;
@@ -33,6 +37,35 @@ export const ProductShareModal: FC<ProductShareModalProps> =
   productDetails,
 }) => {
 
+  const viewShotRef = useRef();
+
+  const handleSnapchatShare = async () => {
+    viewShotRef.current.capture().then(async uri => {
+      console.log("URI", uri)
+      const photoContent = {
+        content: {
+          uri: `file://${uri.trim()}`,
+        },
+        sticker: {
+          uri: `file://${uri.trim()}`,
+          width: 550,
+          height: 900,
+          posX: 0.5,
+          posY: 0.6,
+          rotationDegreesInClockwise: 0,
+          isAnimated: false,
+        },
+        caption: 'Get on lootswap',
+        attachmentUrl: 'https://download.lootswap.com',
+      };
+
+      CreativeKit.sharePhoto(photoContent).then(() => {
+        console.log('done')
+      })
+      .catch(err => console.log(err));
+    });
+  };
+
   return (
     <LSModal
       isVisible={isVisible}
@@ -40,9 +73,18 @@ export const ProductShareModal: FC<ProductShareModalProps> =
       onBackdropPress={onCloseModal}>
       <LSModal.BottomContainer>
         <ModalHeaderText>Share Listing</ModalHeaderText>
+        <View style={{position: 'absolute', bottom: -999999}}>
+        <ViewShot ref={viewShotRef} options={{format: 'jpg', quality: 0.9}}>
+          <LSProductCard
+            item={productDetails}
+            isHorizontalView={true}
+            key={productDetails?._id}
+          />
+        </ViewShot>
+        </View>
         <ModalSubText>Share To</ModalSubText>
         <ScrollView horizontal>
-          <IconTouchable>
+          <IconTouchable onPress={handleSnapchatShare}>
             <SvgXml xml={SNAPCHAT_ICON} width={scale(60)} height={scale(60)} />
             <IconText>Snapchat</IconText>
           </IconTouchable>
