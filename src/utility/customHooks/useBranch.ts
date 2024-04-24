0fb6ce5823ff93bde35816c862a6af342f76b28f
Liam Amadio <liamamadio@gmail.com>
2024-04-24 13:34:33 -0500

    deep linking with product links

--- src/utility/customHooks/useBranch.ts
+++ src/utility/customHooks/useBranch.ts
@@ -1,12 +1,14 @@
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import branch from 'react-native-branch';
-import {saveInstallParams} from '../../redux/modules';
+import {saveInstallParams, getProductDetails} from '../../redux/modules';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
 
 //import {Alert} from 'react-native';
 
 const useBranch = () => {
   const dispatch = useDispatch();
+  const navigation: NavigationProp<any, any> = useNavigation();
 
   useEffect(() => {
     branch.subscribe(async ({error, params}) => {
@@ -14,11 +16,30 @@ const useBranch = () => {
         console.error('Error from Branch: ' + error);
         return;
       }
+      if (params['+clicked_branch_link'] && params['$ios_url']) {
+        console.log('params', params['$ios_url']);
+        const productId = params['$ios_url'].split('/').pop()
+        console.log('PORUCTID', productId);
+        if (productId) {
+          dispatch(
+            getProductDetails(productId, product => {
+              navigation.reset({
+                index: 0,
+                routes: [{name: 'Home'}],
+              });
+              navigation.navigate('ProductDetailsScreen', {
+                productData: product,
+                likedParam: false,
+              });
+            }),
+          );
+
+        }
+      }
       // params will never be null if error is null
       let installParams = await branch.getFirstReferringParams();
       console.log('Branch IO Install Params', installParams);
 
-      //Alert.alert(`${JSON.stringify(installParams)}`);
       // TODO: affiliate codes
       dispatch(
         saveInstallParams({
