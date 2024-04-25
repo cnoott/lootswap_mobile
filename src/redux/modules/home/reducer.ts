0ee98aff2e7031ab58699edae4a56ae9235d1ebe
Liam Amadio <liamamadio@gmail.com>
2024-04-25 16:41:57 -0500

    secondary photos optional chaining

--- src/redux/modules/home/reducer.ts
+++ src/redux/modules/home/reducer.ts
@@ -97,7 +97,7 @@ export default function loading(state = InitialState, action: ActionProps) {
         ...state,
         selectedProductDetails: {
           ...payload,
-          product_photos: [payload.primary_photo, ...payload.secondary_photos],
+          product_photos: [payload?.primary_photo, ...payload?.secondary_photos],
         },
       };
     }
